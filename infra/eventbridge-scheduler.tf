9b8b72eddb8c2c8afe3b0eb55b036473ad938eeb
Dimitris Miaoulis <jimiaoul@gmail.com>
2026-06-24 22:34:12 +0200

    [infra]: fixed fmt error

--- infra/eventbridge-scheduler.tf
+++ infra/eventbridge-scheduler.tf
@@ -2,13 +2,13 @@ module "eventbridge_scheduler" {
   source   = "../modules/eventbridge-scheduler"
   for_each = var.eventbridge_schedulers
 
-  name                       = each.value.name
-  schedule_expression        = each.value.schedule_expression
-  target_arn                 = module.lambda_function[each.value.target_lambda_key].arn
-  role_arn                   = module.iam_role["scheduler_execution"].arn
-  input                      = each.value.input
-  flexible_time_window_mode  = each.value.flexible_time_window_mode
-  state                      = each.value.state
+  name                      = each.value.name
+  schedule_expression       = each.value.schedule_expression
+  target_arn                = module.lambda_function[each.value.target_lambda_key].arn
+  role_arn                  = module.iam_role["scheduler_execution"].arn
+  input                     = each.value.input
+  flexible_time_window_mode = each.value.flexible_time_window_mode
+  state                     = each.value.state
 
   applicationid   = var.applicationid
   applicationname = var.applicationname
