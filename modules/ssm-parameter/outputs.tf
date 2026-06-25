d49a36856ac3e35ed1b8c50f51bcd531273374cc
Dimitris Miaoulis <jimiaoul@gmail.com>
2026-06-25 11:02:14 +0200

    fix: added output

--- modules/ssm-parameter/outputs.tf
+++ modules/ssm-parameter/outputs.tf
@@ -1,3 +1,20 @@
-output "parameter_name" { value = aws_ssm_parameter.ssm_parameter.name }
-output "parameter_arn" { value = aws_ssm_parameter.ssm_parameter.arn }
-output "parameter_version" { value = aws_ssm_parameter.ssm_parameter.version }
+output "parameter_name" { 
+    description = "Name of the SSM parameter"
+    value = aws_ssm_parameter.ssm_parameter.name 
+}
+
+output "parameter_arn" { 
+    description = "ARN of the SSM parameter"
+    value = aws_ssm_parameter.ssm_parameter.arn 
+}
+
+output "parameter_version" { 
+    description = "Version of the SSM parameter"
+    value = aws_ssm_parameter.ssm_parameter.version 
+}
+
+output "parameter_value" {
+  description = "Value of the SSM parameter"
+  value       = aws_ssm_parameter.ssm_parameter.value
+  sensitive   = true
+}
